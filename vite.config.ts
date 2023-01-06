977ec5a27623eb0e150826d384bd3022bed8921e
liuhp <2190098961@qq.com>
2023-01-06 14:52:46 +0800

    'eslint/commitlint'

--- vite.config.ts
+++ vite.config.ts
@@ -5,18 +5,24 @@ import { resolve } from 'path'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  server:{
-    host:'0.0.0.0',
-    port:8080,
-    open:true,
+  server: {
+    host: '0.0.0.0',
+    port: 8080,
+    open: true,
+    proxy: {
+      '/api': {
+        target: 'http://localhost:10010',
+        changeOrigin: true, // 允许跨域
+        rewrite: path => path.replace(/^\/api/, '')
+      }
+    }
   },
-  resolve:{
-    alias:[
+  resolve: {
+    alias: [
       {
         find: '@',
         replacement: resolve(__dirname, 'src')
       }
-    ],
+    ]
   }
-  
 })
